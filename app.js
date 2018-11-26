c903f6cb4b07886cbd5c0c91a14e99fc1eadff2f
Jrm1715 <rocknrollx18@aim.com>
2018-11-25 21:57:40 -0800

    created functions for winning, loosing and drawing.

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const userScpre = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scis_div = document.getElementById("scissors");
+
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = userChoice + " beats " + computerChoice + ". You win!";
+}
+
+function lose() {
+
+}
+
+function draw() {
+
+}
 
 function game(userChoice) {
   const computerChoice = getComputerChoice();
@@ -14,23 +29,23 @@ function game(userChoice) {
     case "rockscis":
     case "paperrock":
     case "scispaper":
-      console.log("User Wins!");
+      win(userChoice, computerChoice);
       break;
-    case "scisrock":
+    case "scissorsrock":
     case "rockpaper":
-    case "paperscis":
-      console.log("Computer Wins!");
+    case "paperscissors":
+      lose(userChoice, computerChoice);
       break;
     case "rockrock":
     case "paperpaper":
-    case "scisscis":
-      console.log("Draw!");
+    case "scissorsscissors":
+      draw(userChoice, computerChoice);
       break;
   }
 }
 
 function getComputerChoice() {
-  const choices = ["rock", "paper", "scis"];
+  const choices = ["rock", "paper", "scissors"];
   const randomNumber = Math.floor(Math.random() * 3);
   return choices[randomNumber];
 }
@@ -43,7 +58,7 @@ function main() {
     game("paper");
   });
   scis_div.addEventListener('click', function() {
-    game("scis");
+    game("scissors");
   });
 }
 
